abc745f556a7dacbcbcc07c71f188663915406f9
GISJohnECS <GISJohnECS@Gmail.com>
2019-12-19 12:46:24 -0600

    Fixed Issue of object of type 'closure' is not subsettable &
    Map Loads points, filters are not functional that is your deliverable.

--- stakingApp/server.R
+++ stakingApp/server.R
@@ -17,8 +17,8 @@ server <- function(input, output, session) {
   #added observe right before the leafletproxy function
   observe(leafletProxy("map", data = filtered_apts()) %>%
             clearMarkers() %>%
-            addCircleMarkers(lng = filtered_apts$Longitude,
-                             lat = filtered_apts$Latitude,
+            addCircleMarkers(lng = data$Longitude, # Research your Error Messages "Error in $: object of type 'closure' is not subsettable" 
+                             lat = data$Latitude,  # We are sending the result Data Frame of filtered_apts() to data, you can't say filtered_apts()$Latitude because its going WTF This is a function not a Data Frame.
                              fillColor  = "blue",
                              color = "black",
                              radius = 14,
@@ -26,47 +26,25 @@ server <- function(input, output, session) {
                              weight = 2,
                              fillOpacity = 0.5,
                              #added in pathOptions
-                             options = pathOptions(pane = "markers"),
-                             popup = paste("<h2>", df$jobnumber,"</h2>", "<br>",  
-                                           "<b>Job Name:</b>", df$jobname, "<br>",  
-                                           "<b>Pole Number:</b>",df$polenumber, "<br>",    
-                                           "<b>Address:</b>", df$houseno," ",df$address," ", "<br>",  
-                                           "<b>Meeting Location:</b>", df$meetinglocation, "<br>",  
-                                           "<b>Appointment Time:</b>",df$appointmenttime, "<br>",  
-                                           "<b>Staker:</b>", df$staker, "<br>",  
-                                           "<b>Appointmet Date:</b>",df$appointmentdate, "<br>"))
-          
-  # got error : Error in $: object of type 'closure' is not subsettable        
-  )
+                             options = pathOptions(pane = "markers"))
+          ) 
   
-  ##Generate Map display
-  # I want to use our custom mapbox style
+  # Bluebonnet Electric Default Basemap
   bec_map <- "https://api.mapbox.com/styles/v1/gisjohnbb/cjmaudm2mha1e2splkup0tc3s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2lzam9obmJiIiwiYSI6ImNqbHptM2g3YTBxcWozdm53bXJrOWxwcWwifQ.3zAsFMWc6_CrtYSvEyNl9w"
-  
   map_attr <- 'John Lister - GIS Applications Developer | Brandon Mitschke - GIS Applications Student'
+  
+  #Render the Leaflet Map
   output$map <- renderLeaflet({
-    df <- data
-    
-    leaflet(data = df) %>%    
+    data <- data
+    leaflet(data = data) %>%    
       setView(-97.285944, 30.104740, 9) %>%
       #added two map panes for each layer
-      addMapPane("iso", zIndex = 420) %>%
-      addMapPane("markers", zIndex = 430) %>%
-      #added another map pane for the appointment table click event
+      addMapPane("iso", zIndex = 420) %>% 
+      addMapPane("markers", zIndex = 430) %>% 
+      #added another map pane for the appointment table click event 
       addMapPane("tblapts", zIndex = 440) %>%
-      addTiles(urlTemplate = bec_map, attribution = map_attr)
+      addTiles(urlTemplate = bec_map, attribution = map_attr) 
     
-  })      
-    
-    
-    #** Output Shit **
-   # print(filtered_apts$jobnumber) #, filtered_apts$Longitude, filtered_apts$Latitude)
-   # print(filtered_apts$Longitude)
-    
-  
-    #----------------------------------------------------------------
-    
-  
-  
+  })
   
 }
